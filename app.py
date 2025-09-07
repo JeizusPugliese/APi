1ec6802373ee9e7caf5ac058efe466f2303581ef
Jesus Pugliese <110912885+JeizusPugliese@users.noreply.github.com>
2025-09-07 18:18:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_connection():
 
 revoked_tokens = set()
 
-@app.route('/')
+@app.route('/home')
 def home():
     return jsonify({"message": "Bienvenido a la API con PostgreSQL!"})
 
@@ -484,3 +484,4 @@ def insertar_medidas():
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=port)
+
